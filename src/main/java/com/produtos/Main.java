a3121b8db887b55677dddf3f622f58eb7cadd8f0
Marginal0304 <luccasdeassis0304@gmail.com>
2025-06-15 20:57:56 -0300

    Teste

--- src/main/java/com/produtos/Main.java
+++ src/main/java/com/produtos/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         stage.setTitle("Estoque de Produtos");
         stage.setScene(scene);
         stage.show();
+        ///aaa
     }
 
     public static void main(String[] args) {
